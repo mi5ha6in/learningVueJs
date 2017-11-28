fdebe54d18019c1b52c5cabb7ef4dc36ae8b83ce
Dmitriy <mi5ha6in@gmail.com>
2017-11-28 23:45:03 +0500

    Vue computed

--- dudar/3/main.js
+++ dudar/3/main.js
@@ -1,13 +1,18 @@
 new Vue ({
   el: '#app',
   data: {
-    value: 1,
-    doubleValue: 1
+    value: 1
   },
   methods: {
     increment (value) {
       this.value = value
-      this.doubleValue = value * 2
+      if (value == 25)
+        alert ("number = 25")
+    }
+  },
+  computed: {
+    doubleValue () {
+      return this.value * 2
     }
   }
 });
