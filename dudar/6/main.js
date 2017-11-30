acbd38853453f9228dc16d7dce0b6e6b43c35813
Dmitriy <mi5ha6in@gmail.com>
2017-11-30 23:58:09 +0500

    Vue component part 2

--- dudar/6/main.js
+++ dudar/6/main.js
@@ -1,17 +1,20 @@
-Vue.filter ('capatilize', function (value) {
-  if (!value) return ''
-  value = value.toString()
-  return value.replace(/\b\w/g, function(l) { return l.toUpperCase() })
+Vue.component ('app-car', {
+  data: function  () {
+    return {
+      cars: [
+        {model: "BMW", speed: 250},
+        {model: "Audi", speed: 240},
+        {model: "Mercedes", speed: 350},
+        {model: "Ford", speed: 160},
+    ]
+    }
+  },
+  template: '<div><div class="car" v-for="car in cars"><p>{{ car.model }}</p></div></div>'
 });
 
 new Vue ({
   el: '#app',
   data: {
-    cars: [
-      {model: "BMW", speed: 250},
-      {model: "Audi", speed: 240},
-      {model: "Mercedes", speed: 350},
-      {model: "Ford", speed: 160},
-    ]
+    
   },
 });
