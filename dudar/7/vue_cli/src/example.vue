7ba207899388a4da450ae08ce4a36c938840bd20
Dmitriy <mi5ha6in@gmail.com>
2017-12-02 00:02:38 +0500

    Vue vue-cli components

---
+++ dudar/7/vue_cli/src/example.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Заголовок H1</h1>
+  </div>
+</template>
--- dudar/7/vue_cli/src/main.js
+++ dudar/7/vue_cli/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import example from './example.vue'
+
+Vue.component ('app-example', example)
 
 new Vue({
   el: '#app',
