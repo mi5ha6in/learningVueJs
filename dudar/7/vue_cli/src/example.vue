b8efa363a742b431446f3f69fc538d0e9471e2a4
Dmitriy <mi5ha6in@gmail.com>
2017-12-02 00:09:46 +0500

    Vue vue-cli part 2

--- dudar/7/vue_cli/src/example.vue
+++ dudar/7/vue_cli/src/example.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
     <h1>Заголовок H1</h1>
+    <app-input></app-input>
   </div>
 </template>
+
+<script>
+  import Input from './field.vue';
+  export default {
+    components: {
+      'app-input': Input
+    }
+  }
+</script>
