6090f5d333f8034e42b598a74ddf1e13df68547b
mi5ha6in <mi5ha6in@gmail.com>
2017-12-25 16:50:19 +0500

    Adds template ItemeComponents

--- learning_vuejs_2/shopping-list/src/components/ItemComponent.vue
+++ learning_vuejs_2/shopping-list/src/components/ItemComponent.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>
-    item
-  </div>
+  <li :class="{ 'removed': item.checked }">
+    <div class="checkbox">
+      <label>
+        <input type="checkbox" v-model="item.checked"> {{ item.text }}
+      </label>
+    </div>
+  </li>
 </template>
 
 <script>
-  export default {
-  }
+export default {};
 </script>
 
 <style scoped>
-
+.removed {
+  color: gray;
+}
+.removed span {
+  text-decoration: line-through;
+}
+li {
+  list-style-type: none;
+}
+li span {
+  margin-left: 5px;
+}
 </style>
