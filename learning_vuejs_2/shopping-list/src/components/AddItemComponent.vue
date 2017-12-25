09279646578a30a160c0f213e1c8b77cb32a00b8
mi5ha6in <mi5ha6in@gmail.com>
2017-12-25 16:23:43 +0500

    Adds template in AddItemComponent

--- learning_vuejs_2/shopping-list/src/components/AddItemComponent.vue
+++ learning_vuejs_2/shopping-list/src/components/AddItemComponent.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    Add items
+    <div class="input-group">
+      <input type="text" class="input form-control"
+        placeholder="add shopping list item">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button">Add!</button>
+      </span>
+    </div>
   </div>
 </template>
 
